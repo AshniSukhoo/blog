874e09a6bec513e5e75629215e4ccfa04f6a3080
Ashni Sukhoo <ashii1709@gmail.com>
2015-11-10 21:04:41 +0400

    first commit

---
+++ Processing/function.php
@@ -0,0 +1,14 @@
+<?php 
+  if($_SERVER['REQUEST_METHOD'] == 'POST'){
+	  
+	  if(isset($_POST['username']) == FALSE || empty($_POST['username'])){
+		  $firstname_error = 'Username is required';
+	  }
+	  
+	  if(isset($_POST['password']) == FALSE || empty($_POST['passsword'])){
+		  $lastname_error = 'Password is required.<br/>';
+		  
+	  }
+	
+  }
+?>
