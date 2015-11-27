2160e579eb0ebf451061196987b74a90f71ed64b
Ashni Sukhoo <ashii1709@gmail.com>
2015-11-27 19:13:15 +0400

    modification

--- views/about-queries.php
+++ views/about-queries.php
@@ -1,27 +1,27 @@
 <!DOCTYPE html>
 <html>
-	<head>
-		<link rel="stylesheet" href="assets/css/aboutstyle.css">
-		<link rel="stylesheet"  href="assets/css/homestyle.css">
-	</head>
+<head>
+	<link rel="stylesheet" href="assets/css/aboutstyle.css">
+	<link rel="stylesheet"  href="assets/css/homestyle.css">
+</head>
 
-	<body>
-		<p> 
-			<h1> What is Pholux?</h1>
-		</p>
+<body>
+	<p> 
+		<h1> What is Pholux?</h1>
+	</p>
 				
-		<p> Pholux is a site that gives you the opportunity to share your memories
+	<p> Pholux is a site that gives you the opportunity to share your memories
 				with people across the world. It allows you to upload files that you want to share with people out there. 
-			<br> Got a pet? Show us how fun it is!</br>
-		</p>
+		<br> Got a pet? Show us how fun it is!</br>
+	</p>
 	
-		<?php 
-			if (!empty($_GET['assets/img'])) {
-			  $path = '/assets/img/big cat.jpg';
-			  if (file_exists($path)) {
-				print '<img src="'.$path.'" alt="sample image" />';
-			  }
+	<?php 
+		if (!empty($_GET['assets/img'])) {
+			$path = '/assets/img/big cat.jpg';
+		if (file_exists($path)) {
+			print '<img src="'.$path.'" alt="sample image" />';
 			}
-		?>
-	</body>
+		}
+	?>
+</body>
 </html>
